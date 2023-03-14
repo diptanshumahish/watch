7bc3d6fa0f52ef5aab7241b84dad2a29e922a2f9
SAGNIK SANYAL <sagniksanyal1502@gmail.com>
2023-03-14 13:34:59 +0530

    login using firebase

---
+++ lib/src/features/authentication/app/controller/login_controller.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:watch/src/features/authentication/app/state/login_state.dart';
+
+import '../../../../../app/errors/failure.dart';
+import '../../../../providers/user_provider.dart';
+import '../../data/auth_api.dart';
+
+/// Provider for the sign up state
+final AutoDisposeStateNotifierProvider<LoginController, LoginState>
+    loginControllerProvider =
+    StateNotifierProvider.autoDispose<LoginController, LoginState>(
+  name: 'LoginControllerProvider',
+  (ref) => LoginController(
+    auth: ref.watch(authAPIProvider),
+    userNotifier: ref.watch(userNotifierProvider.notifier),
+  ),
+);
+
+class LoginController extends StateNotifier<LoginState> {
+  late final AuthAPI _auth;
+  late final UserNotifier _userNotifier;
+  LoginController({required AuthAPI auth, required UserNotifier userNotifier})
+      : _auth = auth,
+        _userNotifier = userNotifier,
+        super(const LoginInitial());
+
+  Future<void> loginWithEmail(
+      {required String email, required String password}) async {
+    state = const LoginLoading();
+    Either<Failure, UserCredential> result = await _auth
+        .signInWithEmailAndPassword(email: email, password: password);
+    return result.fold(
+      (Failure failure) => state = LoginFailure(failure),
+      (UserCredential user) {
+        if (user.user?.uid == null) {
+          state = LoginFailure(Failure(
+              message: 'Unable to get user id',
+              stackTrace: StackTrace.current));
+          return;
+        }
+        _userNotifier.setUserProps(
+          uid: user.user!.uid,
+          displayName: user.user!.displayName,
+          email: user.user!.email,
+        );
+        state = const LoginSuccess();
+      },
+    );
+  }
+}
