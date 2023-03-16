0bcc95c0fd2dd7400125f386ef4ff61e647aa112
SAGNIK SANYAL <sagniksanyal1502@gmail.com>
2023-03-16 21:35:21 +0530

    signin with google #84

--- lib/src/features/authentication/app/controller/login_controller.dart
+++ lib/src/features/authentication/app/controller/login_controller.dart
@@ -1,11 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:watch/src/features/authentication/app/state/login_state.dart';
 
 import '../../../../../app/errors/failure.dart';
 import '../../../../providers/user_provider.dart';
 import '../../data/auth_api.dart';
+import '../state/login_state.dart';
 
 /// Provider for the sign up state
 final AutoDisposeStateNotifierProvider<LoginController, LoginState>
@@ -31,6 +31,28 @@ class LoginController extends StateNotifier<LoginState> {
     state = const LoginLoading();
     Either<Failure, UserCredential> result = await _auth
         .signInWithEmailAndPassword(email: email, password: password);
+    return result.fold(
+      (Failure failure) => state = LoginFailure(failure),
+      (UserCredential user) {
+        if (user.user?.uid == null) {
+          state = LoginFailure(Failure(
+              message: 'Unable to get user id',
+              stackTrace: StackTrace.current));
+          return;
+        }
+        _userNotifier.setUserProps(
+          uid: user.user!.uid,
+          displayName: user.user!.displayName,
+          email: user.user!.email,
+        );
+        state = const LoginSuccess();
+      },
+    );
+  }
+
+  Future<void> loginWithGoogle() async {
+    state = const LoginLoading();
+    Either<Failure, UserCredential> result = await _auth.signInWithGoogle();
     return result.fold(
       (Failure failure) => state = LoginFailure(failure),
       (UserCredential user) {
